42d5e50875b148a815c4190f7eb23a3d7d617246
Tarek Iraqi <tarek.iraqi@ibtikar.net.sa>
2023-01-30 21:13:46 +0200

    add elastic search

--- src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,12 +12,15 @@ public class IndexModel : PageModel
 {
     private readonly CatalogService _catalogService;
     private readonly BasketService _basketService;
+    private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(CatalogService catalogService,
-        BasketService basketService)
+        BasketService basketService,
+        ILogger<IndexModel> logger)
     {
         _catalogService = catalogService;
         _basketService = basketService;
+        _logger = logger;
     }
 
     public IEnumerable<CatalogModel> ProductList { get; set; } = new List<CatalogModel>();
@@ -24,12 +28,15 @@ public class IndexModel : PageModel
     public async Task<IActionResult> OnGetAsync()
     {
         ProductList = await _catalogService.GetCatalog();
+        _logger.LogInformation("Getting products from catalog");
         return Page();
     }
 
     public async Task<IActionResult> OnPostAddToCartAsync(string productId)
     {
         var product = await _catalogService.GetCatalog(productId);
+
+
 
         var userName = "swn";
         var basket = await _basketService.GetBasket(userName);
--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
 
 namespace AspnetRunBasics
 {
@@ -13,6 +16,23 @@ namespace AspnetRunBasics
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, config) =>
+                {
+                    config
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(
+                        new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+                        {
+                            IndexFormat = $"applogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                            AutoRegisterTemplate = true,
+                            NumberOfShards = 2,
+                            NumberOfReplicas = 1
+                        })
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .ReadFrom.Configuration(context.Configuration);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
